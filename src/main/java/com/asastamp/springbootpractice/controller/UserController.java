6b561625329216698af94ee55c7365027e589b48
5830032121-KN <n_kittiphan@hotmail.com>
2024-01-27 13:30:58 +0700

    Init spring boot files

---
+++ src/main/java/com/asastamp/springbootpractice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.asastamp.springbootpractice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping(value="/stamp")
+    public String getUserName() {
+        return "stamp";
+    }
+}
